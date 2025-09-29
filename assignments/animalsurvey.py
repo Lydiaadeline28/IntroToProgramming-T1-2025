86ab913af0d7313bf16feeebbea60a12b718229c
Abraham Jebadoss Lydia <835431@stma.k12.mn.us>
2025-09-29 13:45:58 -0500

    add existing file

--- assignments/animalsurvey.py
+++ assignments/animalsurvey.py
@@ -1,9 +1,9 @@
-favorite = input ("What is your favorite animal? \n> ") 
+favorite = input ("What is your favorite animal? \n> ") # prompting the user to give input
 why = input ("why do you like them? \n> ") 
 where = input ("where do they live? \n> ") 
 fun_fact = input ("how many more water packs? \n> ") 
-print ("Animal survey summary")
-print ("your favorite animal is \n" + favorite)
+print ("Animal survey summary") 
+print ("your favorite animal is \n" + favorite) # take the input and summarizes it
 print ("you like them because they are \n" + why)
 print ("They live in \n" + where)
-print ("A fun fact about yoou favorite animal that\n" + where)
+print ("A fun fact about your favorite animal that\n" + where)
